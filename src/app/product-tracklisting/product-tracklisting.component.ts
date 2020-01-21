fa0b0ccb8e5919953ad801c99c0dd0454e68a244
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-01-21 13:37:48 +0100

    Completed module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-tracklisting',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  constructor() { }
+  albumInfo: Album;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
